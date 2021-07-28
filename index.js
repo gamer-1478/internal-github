56f7c8b9433eef8391745d62d20c7c534ac3f63f
gamer-12748 <aayushgarg2006@gmail.com>
2021-07-28 15:36:06 +0530

    run script with live output

--- index.js
+++ index.js
@@ -1,36 +1,12 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
-
+const {RunScript} = require('./config/utils')
 const { UpdateNginxWithDeploy } = require('./models/NginxUpdate');
 
-var child_process = require('child_process');
-
-console.log("Node Version: ", process.version);
-
-function RunScript() {
-    var spawn = require('child_process').spawn;
-    var process = spawn('python', ["./test.py"], { stdio: 'inherit' });
-
-    if (process.stdout != null) {
-        process.stdout.on('data', function (data) {
-            console.log('stdout: ' + data.toString());
-        });
-    }
-    if (process.stderr != null) {
-        process.stderr.on('data', function (data) {
-            console.log('stderr: ' + data.toString());
-        });
-    }
-
-    process.on('exit', function (code) {
-        console.log('child process exited with code ' + code.toString());
-    });
-
-}
-RunScript()
 app.get('/', (req, res) => {
     res.send('Hello World!')
+    RunScript('python',['./test.py'])
 })
 
 app.listen(port, () => {
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 x = 0
 import time
 import sys
-while x!=10:
+while x!=3:
     x = x+1
     sys.stdout.write("running for the "+str(x)+" th time, After 1000Secs\n")
     #print("running for the",x,"th time, After 1000Secs")
