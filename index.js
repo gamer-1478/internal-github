d1c719fd7a1939808a51fa8dfffe078538548739
gamer-12748 <aayushgarg2006@gmail.com>
2021-07-28 15:02:10 +0530

    live output from spawned process

--- index.js
+++ index.js
@@ -4,9 +4,33 @@ const port = process.env.PORT || 3000
 
 const { UpdateNginxWithDeploy } = require('./models/NginxUpdate');
 
+var child_process = require('child_process');
+
+console.log("Node Version: ", process.version);
+
+function RunScript() {
+    var spawn = require('child_process').spawn;
+    var process = spawn('python', ["./test.py"], { stdio: 'inherit' });
+
+    if (process.stdout != null) {
+        process.stdout.on('data', function (data) {
+            console.log('stdout: ' + data.toString());
+        });
+    }
+    if (process.stderr != null) {
+        process.stderr.on('data', function (data) {
+            console.log('stderr: ' + data.toString());
+        });
+    }
+
+    process.on('exit', function (code) {
+        console.log('child process exited with code ' + code.toString());
+    });
+
+}
+RunScript()
 app.get('/', (req, res) => {
     res.send('Hello World!')
-
 })
 
 app.listen(port, () => {
