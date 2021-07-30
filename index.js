90b0e01c367578b3c8911d35b3c638a705d7aa14
gamer-12748 <aayushgarg2006@gmail.com>
2021-07-30 17:04:36 +0530

    some code

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const path = require('path')
+var expressLayouts = require('express-ejs-layouts');
+
 app.use('/', express.static(path.join(__dirname, 'public')))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
+app.use(expressLayouts);
 
 app.get('/', async (req, res) => {
     res.render('index.ejs')
